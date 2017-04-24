f951fdb2fea87d891e65a599ae4434e2e2e542f1
Justin Dugas <jdugas01@hotmail.fr>
2017-04-24 17:41:48 -0400

    Variables fini

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
 
+prenom=John
+echo $prenom
+
+declare -i age=35
+declare -r age2=33
+
+#on met des guillemets lorsqu'il y a des espaces
+cous="Cours de Linux"
+
+echo Le programme $0 a pris le parametre $1
+.
+
+
