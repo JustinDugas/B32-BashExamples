020dc751191b5e5bf7213e6bbbd6132549438134
Justin Dugas <jdugas01@hotmail.fr>
2017-04-24 17:56:29 -0400

    fini avec conditions jpense

--- 04-condition.sh
+++ 04-condition.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
 
+note=60
 
+if test $note -lt 60
+then
+	echo "you failed!"
+elif test $note -eq 60
+then
+	echo "ouf!cest juste!"
+else
+	echo "alright"
+fi
 
+read -p "entrez une lettre: " lettre
+
+case $lettre in
+	[[:lower:]])
+		echo "Lettre en minuscule:"
+		;;
+	*)
+		echo "Majuscule"
+		;;
+esac
+if test $lettre != "a"
+then 
+	echo "ce nest pas un a"
+fi
 
 
 
