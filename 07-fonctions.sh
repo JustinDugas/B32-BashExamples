2272fbe5d0071af6b2dceb58bc0e1438f86a9310
Justin Dugas <jdugas01@hotmail.fr>
2017-04-26 15:10:52 -0400

    On peut faire des exp regulieres

--- 07-fonctions.sh
+++ 07-fonctions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 maFonction() {
-	echo "salut"
+	echo "salut $1"
 }
 
 maFonction MarcO
--- 08-regexp.sh
+++ 08-regexp.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
 
+# Identique à  read -p ``lettre:`` lettre
+echo -n "Lettre: "
+read lettres
+
+# ~ = regexp!
+if [[ $lettres =~ ^[a-z]+$ ]]
+then 
+	echo "C'est un mot"
+fi
